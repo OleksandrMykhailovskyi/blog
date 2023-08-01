6e5d57bd711aea2e55e588cd7734634abe72d08a
Oleksandr Mykhailovskyi <oleksandr_mykhailovskyi@epam.com>
2023-08-01 23:54:25 +0300

    forms and user accounts

--- django_project/urls.py
+++ django_project/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("blog.urls")), # new
+    path("accounts/", include("django.contrib.auth.urls")), # new
+    path("accounts/", include("accounts.urls")),
 ]
