5c7703813eb6cba0775ec91e2f40ae23141f3da7
Oleksandr Mykhailovskyi <oleksandr_mykhailovskyi@epam.com>
2023-08-02 11:00:35 +0300

    added staticfiles formatting

--- django_project/settings.py
+++ django_project/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"] 
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
+STATICFILES_STORAGE ="django.contrib.staticfiles.storage.StaticFilesStorage" # new
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
